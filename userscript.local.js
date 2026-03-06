9be8a07afd22607ab9e26834e7778b61d8d47664
Stephen Hicks <stephenhicks@gmail.com>
2026-03-05 23:43:38 -0500

    add userscript.local.js for local development

---
+++ userscript.local.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Trail Life Connect Tools (local)
+// @version      2026-03–02
+// @description  Power tools for working with Trail Life Connect
+// @author       Stephen Hicks (stephenhicks@gmail.com)
+// @match        https://www.traillifeconnect.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=traillifeconnect.com
+// @grant        none
+// @require      file:///home/sdh/src/tlc/dist/tlc.js
+// ==/UserScript==
